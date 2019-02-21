59437ba758fedcfd453c8d17f4ea3cf3049ac992
Rachel Wyatt <rw126@evansville.edu>
2019-02-21 13:57:17 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,8 +2,6 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
-      t.belongs_to :passenger, index: true
-      t.belongs_to :taxi, index: true
     end
   end
 end
